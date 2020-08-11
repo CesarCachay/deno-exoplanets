dd7c947b931d2514f669262c23ef85da9de6e268
CesarCachay <cesarcachay1996@gmail.com>
2020-08-11 10:13:24 -0500

    Find habitable planets after filtering the csv file

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a first project using deno which allows to read a csv file
+
+To run the project you need to write in the terminal the following:
+
+deno run --allow-read mod.ts
+
+We use the flag --allow-read because Deno is really focusing in security and permissions.
---
+++ mod.ts
@@ -0,0 +1,40 @@
+import { BufReader } from "https://deno.land/std/io/bufio.ts";
+import { join } from "https://deno.land/std/path/mod.ts";
+import { parse } from "https://deno.land/std/encoding/csv.ts";
+
+interface Planet {
+  [key: string]: string;
+}
+
+const loadPlanetsData = async () => {
+  // We use the function join to build the correct path
+  // because the paths in macOS/Linux/Windows are totally different
+  const path = join(".", "kepler_exoplanets_nasa.csv");
+
+  const file = await Deno.open(path);
+  const bufReader = new BufReader(file);
+  const result = await parse(bufReader, {
+    header: true,
+    comment: "#",
+  });
+
+  // Is necessary to close after use Deno.open otherwise the program will crash.
+  Deno.close(file.rid);
+
+  const planets = (result as Array<Planet>).filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+
+    return planet["koi_disposition"] === "CONFIRMED" &&
+      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
+      stellarMass > 0.78 && stellarMass < 1.04 &&
+      stellarRadius > 0.99 && stellarRadius < 1.01;
+  });
+
+  return planets;
+};
+
+const potentialEarths = await loadPlanetsData();
+
+console.log(`${potentialEarths.length} habitable planets found!`);
